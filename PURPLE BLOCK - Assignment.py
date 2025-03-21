774171e8367056439f0172595ed119ab390eeab9
Mohnish Raghu <140805335+Mohnish-Raghu@users.noreply.github.com>
2025-03-21 12:56:59 +0530

    Update PURPLE BLOCK - Assignment.py

--- PURPLE BLOCK - Assignment.py
+++ PURPLE BLOCK - Assignment.py
@@ -1,3 +1,4 @@
+# Function to get the best buy price and the sell price
 def maxProfit(l):
     buy_price = l[0]
     sell_price = l[0]
@@ -8,6 +9,11 @@ def maxProfit(l):
             sell_price = i
     return buy_price, sell_price
 
+# Input
 l = list(map(int,input("Enter a list of prices: ").split()))
+
+# Function call and variables to store the return values
 buy_price, sell_price = maxProfit(l)
+
+# Output
 print(f"To get the maximum profit, below is the best buy price and sell price.\nBuy Price = {buy_price}\nSell Price = {sell_price}")
